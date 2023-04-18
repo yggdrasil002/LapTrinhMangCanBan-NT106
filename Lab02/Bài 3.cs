d3fc4ebeff395c13fc5a883189864055dccc2dfb
Yggdrasil_002 <huyquah03@gmail.com>
2023-04-18 20:51:19 +0700

    Cap nhat Lab2-TH

--- Lab02/Bài 3.cs
+++ Lab02/Bài 3.cs
@@ -19,6 +19,17 @@ namespace Lab02
             InitializeComponent();
         }
 
+        private void PushArray(string[] a, int i)
+        {
+            string temp;
+            while (i < a.Length)
+            {
+                temp = (a[i+1]);
+                a[i+1] = a[i];
+                a[i] = temp;
+                i++;
+            }
+        }
         private void btnRead_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -34,22 +45,59 @@ namespace Lab02
 
             sr.BaseStream.Position = 0;
             sr.DiscardBufferedData();
-
+            Read(sr, lineCount);
+            fs.Close();
+            sr.Close();
+        }
+        private void Read (StreamReader sr, int lineCount)
+        {
             string line = null;
-            string[] text = null;
-            string delim = "";
+            string[] number = null;
+            string deOperators = " +-*/";
+            string[] operators = null;
+            string deNumber = "0123456789 ";
             for (int i = 0; i < lineCount; i++)
             {
                 line = sr.ReadLine();
                 line.Trim();
-                text = line.Split(null);
-                for (int j = 0; j < text.Length; j++)
-                    MessageBox.Show(text[j]);
-                
+                number = line.Split(deOperators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                operators = line.Split(deNumber.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                int result = int.Parse(number[0]);
+                int temp = 0;
+
+                while (temp < operators.Length)
+                {
+                    for (int j = 0; j < operators.Length; j++)
+                    {
+                        if (operators[j] == "*" && operators[j] == "/")
+                            switch (operators[j])
+                            {
+                                case "*":
+                                    result = int.Parse(number[j]) * int.Parse(number[j + 1]);
+                                    number[j] = result.ToString();
+                                    PushArray(number, j + 1);
+                                    PushArray(operators, j);
+                                    break;
+                                case "/":
+                                    result = int.Parse(number[j]) * int.Parse(number[j + 1]);
+                                    number[j] = result.ToString();
+                                    PushArray(number, j + 1);
+                                    PushArray(operators, j);
+                                    break;
+                            }
+                    }
+                    temp++;
+                }
+                for (int k = 0; k < number.Length; k++)
+                    MessageBox.Show(number[k]);
+                break;
             }
-            fs.Close();
-            sr.Close();
             
+            sr.Close();
+        }
+        private void XuLyNgoac()
+        {
+
         }
     }
 }
