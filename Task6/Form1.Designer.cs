6862a81508fba1902b67bdffa53dc5825d426231
Yggdrasil_002 <huyquah03@gmail.com>
2023-05-29 23:47:21 +0700

    RenewTask6

---
+++ Task6/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Task6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbURL = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.pgbLoad = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txbURL
+            // 
+            this.txbURL.Location = new System.Drawing.Point(15, 15);
+            this.txbURL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txbURL.Name = "txbURL";
+            this.txbURL.Size = new System.Drawing.Size(832, 26);
+            this.txbURL.TabIndex = 0;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(857, 15);
+            this.btnGet.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(112, 27);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // pgbLoad
+            // 
+            this.pgbLoad.Location = new System.Drawing.Point(12, 582);
+            this.pgbLoad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pgbLoad.Name = "pgbLoad";
+            this.pgbLoad.Size = new System.Drawing.Size(959, 42);
+            this.pgbLoad.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(15, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(954, 504);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 635);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pgbLoad);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.txbURL);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbURL;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.ProgressBar pgbLoad;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Task6/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Task6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string URL;
+        private List<string> extractedNews;
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            URL = txbURL.Text.ToString();
+            // Clear the previous extracted news
+            extractedNews = new List<string>();
+            listBox1.Items.Clear();
+
+            // Start the extraction process
+            ExtractNewsAsync();
+        }
+        private async void ExtractNewsAsync()
+        {
+            string url = URL;
+
+            // Create a WebClient to download the webpage content
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    // Download the webpage content as a string
+                    string html = await client.DownloadStringTaskAsync(url);
+
+                    // Parse the HTML using HtmlAgilityPack
+                    HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+                    document.LoadHtml(html);
+
+                    // Extract the news articles from the webpage
+                    var newsNodes = document.DocumentNode.SelectNodes("//article[contains(@class, 'item-news')]");
+
+                    if (newsNodes != null)
+                    {
+                        int totalArticles = newsNodes.Count;
+                        int completedArticles = 0;
+
+                        foreach (var newsNode in newsNodes)
+                        {
+                            // Extract the news article content
+                            string article = newsNode.InnerText;
+
+                            // Add the extracted news article to the list
+                            extractedNews.Add(article);
+
+                            // Update the progress bar
+                            completedArticles++;
+                            int progress = (completedArticles * 100) / totalArticles;
+                            pgbLoad.Value = progress;
+                        }
+
+                        // Display the extracted news articles in the list
+                        foreach (var article in extractedNews)
+                        {
+                            listBox1.Items.Add(article);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No news articles found on the webpage.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize the progress bar
+            pgbLoad.Minimum = 0;
+            pgbLoad.Maximum = 100;
+            pgbLoad.Step = 10;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                string selectedArticle = extractedNews[listBox1.SelectedIndex];
+                
+                string fullArticleUrl = URL + selectedArticle;
+
+                // Open the full article in the default web browser
+                System.Diagnostics.Process.Start(fullArticleUrl);
+            }
+        }
+    }
+}
